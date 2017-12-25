a18b184facbb06d6b44e68debecef3d8696666c0
Anthony McLin <anthony.mclin@rosetta.com>
2017-12-25 14:22:03 -0800

    Setup functions for reading sensor states

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-// var rpio = require('rpio')
-var fs = require('fs')
+var rpio = require('rpio')
 
-// Get settings from config file
-console.log('Reading sensor configs from file.')
-var contents = fs.readFileSync('config.json')
-var config = JSON.parse(contents)
+/**
+ * Loads the config from a file
+ *
+ * @return Object config object
+ **/
+var loadConfig = function () {
+  var fs = require('fs')
+  var contents = fs.readFileSync('config.json')
+  var config = JSON.parse(contents)
+  console.log('Reading sensor configs from file:', config)
+  return config
+}
 
-console.log(config)
+/**
+ * Loops through the configured list of pins and sets
+ * them to a read state
+ */
+var initSensors = function () {
+  config.sensors.forEach(function (el) {
+    rpio.open(el.pin, rpio.INPUT)
+  })
+}
+
+/**
+ * Loops through the configured sensors and returns
+ * their current state
+ *
+ * @param Array List of sensors to ready
+ * @return Array List of objects containing sensors and their states
+ **/
+var readSensors = function (sensors) {
+  var sensorStates = []
+
+  sensors.forEach(function (el) {
+    sensorStates.push({
+      pin: el.pin,
+      state: rpio.read(el.pin)
+    })
+  })
+
+  return sensorStates
+}
+
+var config = loadConfig()
 
 module.exports = function () {
-  config.foo = 'bar'
-  return config
+  return {
+    config: config,
+    readSensors: readSensors,
+    initSensors: initSensors
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 var tap = require('tap')
 var sensor = require('../index.js')
 
-tap.equal(sensor().foo, 'bar')
-
-tap.pass('tests passed.')
+// Loads the config object
+tap.equal(typeof sensor().config, 'object')
